63e499f820010b8fea14787c320007ad6aa013ab
Administrator <390057892@qq.com>
2019-04-23 21:52:49 +0800

    合并代码

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -30,57 +31,91 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() {
     super.initState();
-//    fetchPost();
-    final post = {
-      'title': 'hello',
-      'description': 'hhhhhh',
-    };
-    print(post['title']);
-    print(post['description']);
-
-    final postJson = json.encode(post); //map转json
-
-    print(postJson);
-
-    final postJsonCoverted = json.decode(postJson);
-
-    print(postJsonCoverted['title']);
-    print(postJsonCoverted['description']);
-    print(postJsonCoverted is Map);
-
-    final postModel=Post.fromJson(postJsonCoverted);
-
-    print('title: ${postModel.title}');
-    print('title: ${postModel.description}');
-
-    print('${json.encode(postModel)}');
+    fetchPosts().then((value) => print("aaa: $value"));
+//    final post = {
+//      'title': 'hello',
+//      'description': 'hhhhhh',
+//    };
+//    print(post['title']);
+//    print(post['description']);
+//
+//    final postJson = json.encode(post); //map转json
+//
+//    print(postJson);l
+//
+//    final postJsonCoverted = json.decode(postJson);
+//
+//    print(postJsonCoverted['title']);
+//    print(postJsonCoverted['description']);
+//    print(postJsonCoverted is Map);
+//
+//    final postModel = Post.fromJson(postJsonCoverted);
+//
+//    print('title: ${postModel.title}');
+//    print('title: ${postModel.description}');
+//
+//    print('${json.encode(postModel)}');
   }
 
-  fetchPost() async {
+  Future<List<Post>> fetchPosts() async {
     final response =
         await http.get('https://resources.ninghao.net/demo/posts.json');
-    print('statusCode: ${response.statusCode}');
-    print('body: ${response.body}');
+//    print('statusCode: ${response.statusCode}');
+//    print('body: ${response.body}');
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+          .map<Post>((item) => Post.fromJson(item))
+          .toList();
+      return posts;
+    } else {
+      throw Exception('statusCode: ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: fetchPosts(),
+      builder: (ctx, snapshot) {
+        print('data: ${snapshot.data}');
+        print('data: ${snapshot.connectionState}');
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Text('Loading'),
+          );
+        }
+        return ListView(
+          children: snapshot.data.map<Widget>((item){
+            return ListTile(
+              title: Text(item.title),
+              subtitle: Text(item.author),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(item.imageUrl),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
   }
 }
 
 class Post {
+  final int id;
   final String title;
   final String description;
+  final String author;
+  final String imageUrl;
 
-  Post(this.title, this.description);
+  Post(this.id, this.title, this.description, this.author, this.imageUrl);
 
   Post.fromJson(Map json)
-      : title = json['title'],
-        description = json['description'];
+      : id = json['id'],
+        title = json['title'],
+        description = json['description'],
+        author = json['author'],
+        imageUrl = json['imageUrl'];
 
-  Map toJson() =>{
-    'title':title,
-    'description':description
-  };
+  Map toJson() => {'title': title, 'description': description};
 }
